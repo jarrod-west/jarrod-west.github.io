077623a202728e569503409659f2c9d184fa0b15
Jarrod West <jnw.dev@gmail.com>
2024-06-10 12:35:01 +1000

    Basic header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full text-center border-t border-grey p-4 sticky bg-black text-white bottom-0">
+      Some footer
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full text-center border-b border-grey p-4 sticky bg-black text-white top-0">
+      Some header
+    </header>
+  );
+};
+
+export default Header;
