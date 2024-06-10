8152eafc96a58caa45f3b4f9239b8ea6dc321464
Jarrod West <jnw.dev@gmail.com>
2024-06-10 23:35:31 +1000

    Reactive width

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -7,14 +7,20 @@ export type SectionProps = {
 
 const Section: React.FC<SectionProps> = (props: SectionProps) => {
   return (
-    <div className="w-full p-20 text-white">
-      <div className="font-mono text-5xl pb-10 pt-10 underline">
-        {props.header}
+    <div className="w-full text-white">
+      <div className="flex flex-row">
+        <div className="basis-1/4 max-lg:hidden" />
+        <div className="basis-1/2 max-lg:basis-full pt-20 pb-20 pl-5 pr-5">
+          <div className="font-mono text-5xl pb-10 pt-10 underline">
+            {props.header}
+          </div>
+          <div
+            className="font-mono text-xl"
+            dangerouslySetInnerHTML={{ __html: props.html }}
+          />
+        </div>
+        <div className="basis-1/4 max-lg:hidden" />
       </div>
-      <div
-        className="font-mono text-xl"
-        dangerouslySetInnerHTML={{ __html: props.html }}
-      ></div>
     </div>
   );
 };
