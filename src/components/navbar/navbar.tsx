12c0e6d2129c35cbf58ebca2641f50810a9c023d
Jarrod West <jnw.dev@gmail.com>
2024-06-12 01:48:41 +1000

    Better link styling

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,34 +7,49 @@ import NavbarMenu from "./navbarMenu";
 
 // Page header/navbar
 const Navbar = ({ menuItems }: NavbarProps) => {
-
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   return (
     <>
-    <nav className="relative px-4 py-4 flex justify-between max-lg:justify-end items-center border-b border-grey bg-black text-white h-20">
-      {/* Hamburger button, only shown for small screens */}
-      <div className="lg:hidden">
-        <button className="navbar-burger flex items-center p-3" onClick={() => setMenuOpen(!menuOpen)}>
-          <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Mobile menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-          </svg>
-        </button>
-      </div>
-      {/* Hamburger button, only shown for small screens */}
-      <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
-        {menuItems.map((menuItem) => (
-          <li>
-            <Link className="text-gray-400 hover:text-white text-xl"
-              to={menuItem.slug}>{menuItem.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-    {/* Menu from opening hamburger button */}
-    { menuOpen ? <NavbarMenu menuItems={menuItems} closeMenu={() => setMenuOpen(false)} /> : null }
+      <nav className="relative px-4 py-4 flex justify-between max-lg:justify-end items-center border-b border-grey bg-black text-white h-20">
+        {/* Hamburger button, only shown for small screens */}
+        <div className="lg:hidden">
+          <button
+            className="navbar-burger flex items-center p-3"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <svg
+              className="block h-4 w-4 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Mobile menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
+        </div>
+        {/* Hamburger button, only shown for small screens */}
+        <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+          {menuItems.map((menuItem) => (
+            <li>
+              <Link
+                className="text-gray-400 hover:text-white text-xl"
+                to={menuItem.slug}
+                activeClassName="text-gray-700 hover:text-white text-xl"
+              >
+                {menuItem.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      {/* Menu from opening hamburger button */}
+      {menuOpen ? (
+        <NavbarMenu
+          menuItems={menuItems}
+          closeMenu={() => setMenuOpen(false)}
+        />
+      ) : null}
     </>
   );
 };
--- src/components/navbar/navbarMenu.tsx
+++ src/components/navbar/navbarMenu.tsx
@@ -3,15 +3,29 @@ import { Link } from "gatsby";
 
 import { NavbarMenuProps } from "./types";
 
-const NavbarMenu = ({menuItems, closeMenu}: NavbarMenuProps) => {
+const NavbarMenu = ({ menuItems, closeMenu }: NavbarMenuProps) => {
   return (
     <div className="navbar-menu relative z-50">
-      <div className="navbar-backdrop fixed inset-0 bg-gray-500 opacity-25" onClick={() => closeMenu()}></div>
+      <div
+        className="navbar-backdrop fixed inset-0 bg-gray-500 opacity-25"
+        onClick={() => closeMenu()}
+      ></div>
       <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-black border-r overflow-y-auto">
         <div className="flex items-center mb-8">
           <button className="navbar-close" onClick={() => closeMenu()}>
-            <svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            <svg
+              className="h-6 w-6 text-gray-400 cursor-pointer hover:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
             </svg>
           </button>
         </div>
@@ -19,8 +33,12 @@ const NavbarMenu = ({menuItems, closeMenu}: NavbarMenuProps) => {
           <ul>
             {menuItems.map((menuItem) => (
               <li className="mb-1">
-                <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-black rounded"
-                  to={menuItem.slug}>{menuItem.title}
+                <Link
+                  className="block p-4 text-sm font-semibold text-gray-400 hover:bg-gray-300 hover:text-black rounded"
+                  to={menuItem.slug}
+                  activeClassName="text-gray-700 hover:bg-gray-300 hover:text-black rounded"
+                >
+                  {menuItem.title}
                 </Link>
               </li>
             ))}
@@ -29,7 +47,6 @@ const NavbarMenu = ({menuItems, closeMenu}: NavbarMenuProps) => {
       </nav>
     </div>
   );
-}
-
+};
 
 export default NavbarMenu;
